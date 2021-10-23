b2b083a657c8b5948ad72d07568d5b5fdb7745cf
zTags <wierdgeomery@gmail.com>
2021-10-23 19:28:44 +0200

    switch keyboard libs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 
 mod vga_buffer;
+mod recovery;
 
 use core::panic::PanicInfo;
 
@@ -15,45 +16,8 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[no_mangle]
 #[allow(unreachable_code)]
-pub extern "C" fn _start() -> ! {
-
-    println!("loading keyboard...");
-    
-    let mut controller = unsafe { Controller::new() };
-
-    // disable devices because apparently you need to do that
-    controller.disable_keyboard().unwrap();
-    let _ = controller.read_data();
-    
-    // configure only keyboard
-    let mut config = controller.read_config().unwrap();
-    config.set(
-        ControllerConfigFlags::ENABLE_KEYBOARD_INTERRUPT
-        | ControllerConfigFlags::DISABLE_MOUSE
-        | ControllerConfigFlags::ENABLE_TRANSLATE,
-        false,
-    );
-    controller.write_config(config).unwrap();
-    controller.test_controller().unwrap();
-    // rewrite config if a reset happened
-    controller.write_config(config);
-
-    // test the keyboard
-    let keyboard_ok_question_mark = controller.test_keyboard().is_ok();
-    config = controller.read_config().unwrap();
-    if keyboard_ok_question_mark {
-        controller.enable_keyboard().unwrap();
-        config.set(ControllerConfigFlags::DISABLE_KEYBOARD, false);
-        config.set(ControllerConfigFlags::ENABLE_KEYBOARD_INTERRUPT, true);
-        controller.keyboard().reset_and_self_test().unwrap();
-    } else {
-        panic!("failed to initialize your keyboard, quitting...");
-    }
-
-    let mut keyboard = controller.keyboard();
-
-    println!("keyboard loaded.");
-
+pub extern "C" fn _start() -> ! {;
+    // diffrent keyboard system soon tm
     vga_buffer::clear_scr();
 
     println!("
@@ -70,9 +34,5 @@ pub extern "C" fn _start() -> ! {
                               ");
 
     println!("Arc loaded.");
-    println!("there will be more here soon");
-
-    
-
     loop { }
 }
